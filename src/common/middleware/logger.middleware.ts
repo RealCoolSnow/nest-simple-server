0d26a98795dddc61fbc26facf835093e9de1b114
CoolSnow <coolsnow2020@gmail.com>
2022-02-22 17:05:26 +0800

    feat: logger print client ip

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,10 +1,12 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common'
 import { NextFunction, Request, Response } from 'express'
+import { getClientIp } from 'request-ip'
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    Logger.log(`Request: ${req.headers['user-agent']}`)
+    const clientIp = getClientIp(req)
+    Logger.log(`Request: ${clientIp} ${req.headers['user-agent']}`)
     next()
   }
 }
